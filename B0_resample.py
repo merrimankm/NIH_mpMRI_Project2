8bf0396e1543740eac104a543d53d7a8eb6d2964
merrimankm <katie.merriman@nih.gov>
2022-05-04 13:06:49 -0500

    Initial commit

---
+++ B0_resample.py
@@ -0,0 +1,187 @@
+B0_resample
+#   Created 5/4/22 by Katie Merriman
+#   Searches through designated folder for DICOM files of all patients on csv list
+#   Converts DICOMs to Nifti, determines DICOM type, and saves to designated folder
+#   Resamples B0 based on T2
+#   If ADC NIfTI with same resampling exists, high B can be calculated with calculate_highB.py after running this code
+
+# Requirements
+#   pydicom
+#   SimpleITK
+#   Pandas
+
+# Inputs
+#   1: csv file, including path.
+#           Must include
+#               a. column of MRNs labeled as "MRN"  (MRNs can be JUST the MRN or in MRN_DateOfMRI format)
+#               b. column with filepath to patient folder containing T2 DICOMS labeled "topSourceFolder"
+#               c. column with filepath to folder containing highB DICOMS labeled "bSourceFolder"
+#   2: path to folder where converted files and csv reports should be saved
+#   example usage:
+#   python highB_resample.py "/path/to/cvsFileName.csv" "/path/to/save/folder"
+
+# Outputs
+#   Nifti files saved as T2, ADC, or highB in folders created for each patient within designated save folder
+
+
+import os
+#import sys
+import pydicom
+from pydicom import dcmread
+from pydicom.errors import InvalidDicomError
+import SimpleITK as sitk
+#import csv
+#import glob
+import pandas as pd
+
+
+class resample_highb():
+    def __init__(self):
+        self.csv_file = r'T:\MIP\Katie_Merriman\Project2Data\Patient_list_directories_short.csv'
+        self.patientFolder = r'T:\MIP\Katie_Merriman\Project1Data\PatientNifti_data'
+
+
+        # self.csv_file = 'Mdrive_mount/MIP/Katie_Merriman/Project2Data/Patient_list_directories_short2.csv'
+        # self.patientFolder = 'Mdrive_mount/MIP/Katie_Merriman/Project1Data/PatientNifti_data'
+
+
+    def resampleAll(self):
+        df_csv = pd.read_csv(self.csv_file, sep=',', header=0)
+        patient = []
+        endFolder = []
+        for rows, file_i in df_csv.iterrows():
+            p = (str(file_i['topSourceFolder']))
+            p2 = os.path.join(self.patientFolder, os.path.basename(p), "NIfTI")
+            p3 = (str(file_i['bSourceFolder']))
+            patient.append([p,p2,p3])
+
+        for i in range(0,len(patient)):
+            patient[i][2] = self.readDicoms(patient[i])
+
+    def readDicoms(self, p):
+        success = 0
+        dicomFoldersList = []
+        dicomSeriesList = []
+        dicomProtocolList = []
+
+        for root, dirs, files in os.walk(p[0]):
+            for name in files:
+                filePath = os.path.join(root, name)
+                try:
+                    ds = dcmread(filePath)
+                except IOError:
+                    # print(f'No such file')
+                    continue
+                except InvalidDicomError:
+                    # print(f'Invalid Dicom file')
+                    continue
+                if name != ("DICOMDIR" or "LOCKFILE" or "VERSION"):
+                    dicomString = filePath[:-(len(name) + 1)]
+                    if dicomString not in dicomFoldersList:
+                        if 'delete' in dicomString:
+                             break
+                        dicomFoldersList.append(dicomString)
+                        dicomSeries = ds.ProtocolName.replace('/', '-')
+                        dicomSeries = dicomSeries.replace(" ", "_")
+                        # print(f'DICOM found...')
+                        ADClist = ['Apparent Diffusion Coefficient', 'adc', 'ADC', 'dWIP', 'dSSh', 'dReg']
+                        if ('T2' in dicomSeries or 't2' in dicomSeries):
+                            dicomSeriesType = 'T2'
+                        elif (any([substring in dicomString for substring in ADClist])) or (
+                        any([substring in dicomSeries for substring in ADClist])):
+                            dicomSeriesType = 'ADC'
+                        elif ('extracted' in filePath):
+                            dicomSeriesType = 'B0_extracted'
+                        else:
+                            series_descript = ds.SeriesDescription
+                            if any([substring in series_descript for substring in ADClist]) or (
+                                    dicomString.endswith('ADC') or dicomString.endswith('adc')):
+                                dicomSeriesType = 'ADC'
+                            elif ('T2' in series_descript or 't2' in series_descript) or (
+                                    dicomString.endswith('T2') or dicomString.endswith('t2')):
+                                dicomSeriesType = 'T2'
+                            elif (dicomString.endswith('DCE') or dicomString.endswith('dce')):
+                                dicomSeriesType = 'DCE'
+                            else:
+                                dicomSeriesType = 'highB'
+                        dicomSeriesList.append(dicomSeries)
+                        dicomProtocolList.append(dicomSeriesType)
+
+        for root, dirs, files in os.walk(p[2]):
+            for name in files:
+                filePath = os.path.join(root, name)
+                try:
+                    ds = dcmread(filePath)
+                except IOError:
+                    # print(f'No such file')
+                    continue
+                except InvalidDicomError:
+                    # print(f'Invalid Dicom file')
+                    continue
+                if name != ("DICOMDIR" or "LOCKFILE" or "VERSION"):
+                    dicomString = filePath[:-(len(name) + 1)]
+                    if dicomString not in dicomFoldersList:
+                        if 'delete' in dicomString:
+                             break
+                        dicomFoldersList.append(dicomString)
+                        dicomSeries = ds.ProtocolName.replace('/', '-')
+                        dicomSeries = dicomSeries.replace(" ", "_")
+                        dicomSeriesType = 'B0_extracted'
+                        dicomSeriesList.append(dicomSeries)
+                        dicomProtocolList.append(dicomSeriesType)
+
+
+        if dicomFoldersList:
+            print("Resampling B0")
+            success = self.resample(dicomFoldersList, dicomSeriesList, dicomProtocolList, p)
+
+        return success
+
+    def resample(self, dicomFoldersList, dicomSeriesList, dicomProtocolList, p):
+        success = 0
+        highB_img = []
+        for refPath, DCMseries, dicomProtocol in zip(dicomFoldersList, dicomSeriesList, dicomProtocolList):
+
+            # read original dicom
+            reader = sitk.ImageSeriesReader()
+            # print(f'Reading images...')
+            dicom_names = reader.GetGDCMSeriesFileNames(refPath)
+
+            # convert to NIfTI
+            reader.SetFileNames(dicom_names)
+            # print(f'Converting files...')
+            if dicomProtocol == 'B0_extracted':
+                B0_img = reader.Execute()
+                niiB0 = os.path.join(p[1], "b0_Resampled.nii.gz")
+                # print(f'Converting ADC image')
+            else:
+                if dicomProtocol == 'T2':
+                    t2_img = reader.Execute()
+
+
+        if B0_img:
+            # Resample highB
+            # set the resample filter based on T2 header info
+            Filter = sitk.ResampleImageFilter()
+            Filter.SetReferenceImage(t2_img)
+            Filter.SetOutputDirection(t2_img.GetDirection())
+            Filter.SetOutputOrigin(t2_img.GetOrigin())
+            Filter.SetOutputSpacing(t2_img.GetSpacing())
+
+            # execute
+            B0_resamp = Filter.Execute(B0_img)
+            B0_resamp.CopyInformation(t2_img)
+
+            # make sure all header info matches
+            for meta_elem in t2_img.GetMetaDataKeys():
+                B0_resamp.SetMetaData(meta_elem, t2_img.GetMetaData(meta_elem))
+
+            sitk.WriteImage(B0_resamp, niiB0)
+            success = 1
+        return success
+
+
+if __name__ == '__main__':
+    c = resample_highb()
+    c.resampleAll()
+
